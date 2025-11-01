75704f085aa7fc76656cdfa54b9ac6e80f57b5b8
Sujay S <s.sujay@hotmail.com>
2025-11-01 13:55:55 +0530

    Integrated JWT-based authentication

--- src/main/java/com/sujay/journalApplication/Config/SpringSecurity.java
+++ src/main/java/com/sujay/journalApplication/Config/SpringSecurity.java
@@ -1,10 +1,12 @@
 package com.sujay.journalApplication.Config;
 
+import com.sujay.journalApplication.Filter.JwtFilter;
 import com.sujay.journalApplication.Service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -21,16 +24,21 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
 
+    @Autowired
+    private JwtFilter jwtFilter;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/journal/**", "/user/**").authenticated()
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .anyRequest().permitAll()
+                .anyRequest().permitAll();
+
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .httpBasic();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .csrf().disable();
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
@@ -41,5 +49,11 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
--- src/main/java/com/sujay/journalApplication/Controller/PublicController.java
+++ src/main/java/com/sujay/journalApplication/Controller/PublicController.java
@@ -1,10 +1,19 @@
 package com.sujay.journalApplication.Controller;
 
 import com.sujay.journalApplication.Entity.User;
+import com.sujay.journalApplication.Service.UserDetailsServiceImpl;
 import com.sujay.journalApplication.Service.UserService;
+import com.sujay.journalApplication.Utils.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/public")
 public class PublicController {
@@ -12,13 +21,37 @@ public class PublicController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
     @GetMapping("/health-check")
     public String healthCheck() {
-        return "Ok - Working fine!!!";
+        return "Health check passed – Journal Application is working.";
     }
 
-    @PostMapping("/create-user")
-    public void createUser(@RequestBody User user) {
+    @PostMapping("/signup")
+    public void signup(@RequestBody User user) {
         userService.saveNewUser(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword())
+            );
+            UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
+            String jwtToken = jwtUtil.generateToken(userDetails.getUsername());
+            return new ResponseEntity<>(jwtToken, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Exception occurred while creating Authentication Token ", e);
+            return new ResponseEntity<>("Incorrect userName or Password", HttpStatus.BAD_REQUEST);
+        }
     }
 }
