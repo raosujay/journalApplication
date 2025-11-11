a0db55e2677d65780265e53a9e4737f3ec877791
Sujay S <s.sujay@hotmail.com>
2025-11-11 23:45:27 +0530

    enable transaction management

--- src/main/java/com/sujay/journalApplication/JournalApplication.java
+++ src/main/java/com/sujay/journalApplication/JournalApplication.java
@@ -4,16 +4,25 @@ package com.sujay.journalApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableTransactionManagement
 public class JournalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JournalApplication.class, args);
+    }
+
+    @Bean
+    public PlatformTransactionManager manage(MongoDatabaseFactory dbFactory){
+        return new MongoTransactionManager(dbFactory);
     }
 
     @Bean
